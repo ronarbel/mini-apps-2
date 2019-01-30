daed5cac2acfd43d301bb7cbfe79caa6f96a00b8
Ron Arbel <ronarbel1@gmail.com>
2019-01-30 15:59:21 -0800

    initialize React stateful app

---
+++ challenge_1/client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  render() {
+    return (
+      <h2>hello world 2</h2>
+    );
+  }
+}
+
+export default App;
