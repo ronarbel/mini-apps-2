fa7a4c82092c410c151078253b549c68a6c45333
Ron Arbel <ronarbel1@gmail.com>
2019-01-30 18:06:00 -0800

    limit search result to 10 entries

--- challenge_1/client/components/App.jsx
+++ challenge_1/client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar.jsx';
 import EventList from './EventList.jsx';
+import ReactPaginate from 'react-paginate';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       events: [],
+      pageCount: 0,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,7 +17,7 @@ class App extends React.Component {
   }
 
   getEvents(text) {
-    axios.get(`/events?q=${text}`)
+    axios.get(`/events?q=${text}&_limit=${10}`)
       .then((events) => {
         console.log(events.data);
         this.setState({ events: events.data });
@@ -30,12 +32,26 @@ class App extends React.Component {
     };
   }
 
+
   render() {
-    const { events } = this.state;
+    const { events, pageCount } = this.state;
     return (
       <div>
         <SearchBar handleSubmit={this.handleSubmit} />
         <EventList events={events} />
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
       </div>
     );
   }
