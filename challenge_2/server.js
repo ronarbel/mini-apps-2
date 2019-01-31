b7f6be10f001dd81b917d67077a798bc62c2f16f
Ron Arbel <ronarbel1@gmail.com>
2019-01-31 11:42:20 -0800

    Fetch default bpi data on componentDidMount

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
 // -------- middleware -------- //
+app.use(express.static('public'));
 app.use(morgan('short'));
 
 // -------- crud -------- //
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/prices', (req, res) => {
+  const CURRENCY = 'bpi';
+  
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+    .then(response => res.send(response.data[CURRENCY]))
+    .catch(err => res.status(500).send(err));
+});
 
 // -------- server -------- //
 const port = 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`CryptoViewer listening on port ${port}!`));
