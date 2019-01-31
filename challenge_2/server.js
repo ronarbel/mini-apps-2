430eb1064977947ca272509fa7aee68014087637
Ron Arbel <ronarbel1@gmail.com>
2019-01-31 10:36:50 -0800

    Initialize webpack and express

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
 
+const app = express();
 
+// -------- middleware -------- //
+app.use(morgan('short'));
+
+// -------- crud -------- //
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// -------- server -------- //
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
