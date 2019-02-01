431da7d3975fadb25f2888cc223c704e0de81d44
Ron Arbel <ronarbel1@gmail.com>
2019-02-01 13:19:14 -0800

    Add TotalScore component

--- challenge_3/client/components/ScoreTable.jsx
+++ challenge_3/client/components/ScoreTable.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Frame from './Frame.jsx';
+import PropTypes from 'prop-types';
+import TotalScore from './TotalScore.jsx';
 
 const ScoreTable = ({ throws }) => {
+  let totalScore = 0;
+  const frames = throws.map((value, i) => {
 
+  })
   return (
     <div className="score-table">
       <Frame />
+      <TotalScore totalScore={totalScore} />
     </div>
   );
+};
+
+ScoreTable.propTypes = {
+  throws: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default ScoreTable;
